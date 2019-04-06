f5e919df77dd1a35d7bd67e46a04aae887847d86
Raul de Paula <raul.p.p@hotmail.com>
2019-04-06 11:59:27 -0300

    Merge origin/master
    Conflicts:
    	src/main/java/br/senac/tads/pi3a/livrariatades/servico/produto/ProdutosDash.java

---
+++ src/main/java/br/com/jrl/Jrl/JrlApplication.java
@@ -0,0 +1,13 @@
+package br.com.jrl.Jrl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrlApplication.class, args);
+	}
+
+}
