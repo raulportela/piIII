551097d5e390c6e171d8d0e5b503fac6b80bcd28
Raul de Paula <raul.p.p@hotmail.com>
2019-04-06 12:06:18 -0300

    Merge origin/master
    Conflicts:
    	src/main/java/br/senac/tads/pi3a/livrariatades/servico/produto/ProdutosDash.java

---
+++ src/main/java/br/com/jrl/Jrl/dao/cliente/DaoCliente.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.jrl.Jrl.dao.cliente;
+
+/**
+ *
+ * @author Raul Portela
+ */
+public class DaoCliente {
+
+    public static boolean inserir() {
+        return true;
+    }
+}
